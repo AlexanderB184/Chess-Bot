2dc4ce04ccc920ca089a2b7f55515874c3369d7f
AlexanderB184 <109999594+AlexanderB184@users.noreply.github.com>
2024-07-05 14:26:10 +0930

    Delete bots directory

--- bots/minmax.h
+++
@@ -1,4 +0,0 @@
-#include "../chess.h"
-
-Move best_move(const Board* board);
-Move best_move_old(const Board* board);
--- bots/randbot.h
+++
@@ -1,4 +0,0 @@
-#include <stdlib.h>
-
-#include "../chess.h"
-Move rand_move(const Board*);
