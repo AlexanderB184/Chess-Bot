2d808a15cc48dfcec58dd98830eb917f49505823
AlexanderB184 <a1849988@adelaide.edu.au>
2024-10-31 23:04:37 +1030

    adding datatypes for SEE

--- chess-bot/include/see.h
+++ chess-bot/include/see.h
@@ -3,6 +3,18 @@
 
 #include "bot.h"
 
+typedef struct {
+  score_cp_t value;
+  sq0x88_t square;
+  uint8_t padding;
+} attacker_t;
+
+typedef struct {
+  int piece_count;
+  attacker_t pieces[16];
+} attacker_list_t;
+
+
 score_cp_t static_exchange_evaluation(const chess_state_t* chess_state,
                                              move_t move);
 
