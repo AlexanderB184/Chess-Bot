e0244484b213e35f256e3df956a1fbe7789d715f
AlexanderB184 <a1849988@adelaide.edu.au>
2024-11-01 00:22:14 +1030

    changed moved compression

--- chess-bot/src/transposition_table.c
+++ chess-bot/src/transposition_table.c
@@ -10,8 +10,7 @@
 
 move_t entry_best_move(entry_t entry) {
   uint16_t compressed_move = ((entry >> 48) & 0xFFFF);
-  return move(sq8x8_to_sq0x88(compressed_move & 0x3F), sq8x8_to_sq0x88(((compressed_move) >> 6) & 0x3F),
-              ((compressed_move >> 12) & 0xF));
+  return uncompress_move(compressed_move);
 }
 
 score_cp_t entry_score(entry_t entry) {
@@ -29,10 +28,9 @@ int entry_age(entry_t entry) { return (int)((entry >> 24) & 0xFF); }
 entry_t make_entry(enum tt_entry_type type, move_t best_move, score_cp_t score,
                    int depth, int age) {
   return (entry_t)((entry_t)(type & 0b11) | ((entry_t)(age & 0xFF) << 24) |
-                   ((entry_t)(depth & 0xFF) << 16) | ((entry_t)(score & 0xFFFF) << 32) |
-                   (((entry_t)(sq0x88_to_sq8x8(best_move.from) & 0x3F) | ((entry_t)(sq0x88_to_sq8x8(best_move.to) & 0x3F) << 6) |
-                     ((entry_t)(best_move.bitpacked_data & 0xF) << 12))
-                    << 48));
+                   ((entry_t)(depth & 0xFF) << 16) |
+                   ((entry_t)(score & 0xFFFF) << 32) |
+                   ((entry_t)compress_move(best_move) << 48));
 }
 
 void tt_init(table_t* table, uint64_t size) {
@@ -44,37 +42,41 @@ void tt_init(table_t* table, uint64_t size) {
 void tt_free(table_t* table) { free((void*)table->items); }
 
 entry_t tt_get(table_t* table, zobrist_t key) {
-    uint64_t index = key % table->capacity;
-    zobrist_t stored_key = table->items[index].key;
-    uint64_t stored_entry = table->items[index].entry;
-    if ((stored_key ^ stored_entry) == key) {
-        return stored_entry;
-    }
-    return (entry_t)0;
+  uint64_t index = key % table->capacity;
+  zobrist_t stored_key = table->items[index].key;
+  uint64_t stored_entry = table->items[index].entry;
+  if ((stored_key ^ stored_entry) == key) {
+    return stored_entry;
+  }
+  return (entry_t)0;
 }
 
-void tt_store(table_t* table, zobrist_t key, enum tt_entry_type type, move_t best_move, score_cp_t score, int depth, int age) {
+void tt_store(table_t* table, zobrist_t key, enum tt_entry_type type,
+              move_t best_move, score_cp_t score, int depth, int age) {
+  uint64_t index = key % table->capacity;
+  entry_t entry = make_entry(type, best_move, score, depth, age);
+  table->items[index].key = key ^ entry;
+  table->items[index].entry = entry;
+}
+
+void tt_store_depth_prefered(table_t* table, zobrist_t key,
+                             enum tt_entry_type type, move_t best_move,
+                             score_cp_t score, int depth, int age) {
+  uint64_t index = key % table->capacity;
+  uint64_t stored_entry = table->items[index].entry;
+  if (!stored_entry || entry_depth(stored_entry) < depth) {
+    entry_t entry = make_entry(type, best_move, score, depth, age);
+    table->items[index].key = key ^ entry;
+    table->items[index].entry = entry;
+  }
+}
+
+void tt_store_pv(table_t* table, zobrist_t key, enum tt_entry_type type,
+                 move_t best_move, score_cp_t score, int depth, int age) {
+  if (type == TT_EXACT) {
     uint64_t index = key % table->capacity;
     entry_t entry = make_entry(type, best_move, score, depth, age);
     table->items[index].key = key ^ entry;
     table->items[index].entry = entry;
-}
-
-void tt_store_depth_prefered(table_t* table, zobrist_t key, enum tt_entry_type type, move_t best_move, score_cp_t score, int depth, int age) {
-    uint64_t index = key % table->capacity;
-    uint64_t stored_entry = table->items[index].entry;
-    if (!stored_entry || entry_depth(stored_entry) < depth) {
-        entry_t entry = make_entry(type, best_move, score, depth, age);
-        table->items[index].key = key ^ entry;
-        table->items[index].entry = entry;
-    }
-}
-
-void tt_store_pv(table_t* table, zobrist_t key, enum tt_entry_type type, move_t best_move, score_cp_t score, int depth, int age) {
-    if (type == TT_EXACT) {
-        uint64_t index = key % table->capacity;
-        entry_t entry = make_entry(type, best_move, score, depth, age);
-        table->items[index].key = key ^ entry;
-        table->items[index].entry = entry;
-    }
+  }
 }
